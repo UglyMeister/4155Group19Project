9b2ad4cf200bb67124f70c57d363ea795fc1d6b5
muma10837 <jmuma@uncc.edu>
2020-03-13 15:55:01 -0400

    a

--- util/db.js
+++ util/db.js
@@ -1,26 +1,25 @@
-var getAll = async function(db){
+getAll = async function(db) {
     let promise = await db.find({}).exec();
     return promise;
 };
 
-var getOne = async function(db, uname){
-    let promise = await db.find({'uname': uname}).exec();
+var getOne = async function(db, uname) {
+    let promise = await db.find({ uname: uname }).exec();
     return promise;
 };
 
-var oneExists = function(uname){
-    if(getOne)
-    return true;
-    else
-    return false;
+var oneExists = function(uname) {
+    if (getOne) return true;
+    else return false;
 };
 
-var addNewUser = async function(db){
+var addNewUser = async function(db) {
     let promise = await db.save();
     return promise;
-}
+};
 
 module.exports.getAll = getAll;
 module.exports.getOne = getOne;
 module.exports.oneExists = oneExists;
 module.exports.addNewUser = addNewUser;
+
