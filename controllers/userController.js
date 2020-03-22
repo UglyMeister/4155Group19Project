12e7f7a85ad64ea13a214cebeedd9f8e450b09bb
muma10837 <jmuma@uncc.edu>
2020-03-21 21:03:41 -0400

    profile.ejs done needs testing

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,10 +2,15 @@ const EmployeeModel = require('./../models/employee');
 
 exports.getProfile = async (req, res, next) => {
     try {
-        console.log(req.session.profile);
         if (req.session.profile) {
-            console.log('render profile');
-            res.render('profile', { data: req.session.profile.name }); //this is temporary, will change later
+            //find all groups by id and put names here
+            const groups = null;
+            const numberOfGroups = 0;
+            res.render('profile', {
+                type: req.session.type,
+                groups: groups,
+                numberOfGroups: numberOfGroups
+            }); //this is temporary, will change later
         } else {
             res.render('index', { data: 'error, not logged in, please log in' });
         }
@@ -13,4 +18,3 @@ exports.getProfile = async (req, res, next) => {
         console.log(e);
     }
 };
-
