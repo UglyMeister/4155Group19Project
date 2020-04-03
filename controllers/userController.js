62cec7d6fed48c0ea7f71c611ead24eeea4160c2
muma10837 <jmuma@uncc.edu>
2020-04-03 16:56:49 -0400

    added some beginning error catching

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,15 +10,19 @@ exports.getProfile = async (req, res, next) => {
                 { _id: { $in: req.session.profile.groupIDs } },
                 { name: 1 }
             );
+            req.session.save();
             res.render('profile', {
                 type: req.session.type,
                 groups: req.session.groupNames,
                 numberOfGroups: req.session.groupNames.length,
                 name: req.session.profile.name,
                 loggedIn: req.session.loggedIn
-            }); //this is temporary, will change later
+            });
         } else {
-            res.render('index', { data: 'error, not logged in, please log in', loggedIn: req.session.loggedIn });
+            res.render('index', {
+                data: 'error, not logged in, please log in',
+                loggedIn: req.session.loggedIn
+            });
         }
     } catch (e) {
         console.log(e);
@@ -27,42 +31,80 @@ exports.getProfile = async (req, res, next) => {
 
 exports.createJob = async (req, res, next) => {
     try {
-        const employer = req.session.profile;
-        req.body.ownerId = employer._id;
-        const newGroup = await GroupModel.create(req.body);
-        req.session.groupNames.push(newGroup);
-        await EmployerModel.findByIdAndUpdate(employer._id, { $push: { groupIDs: newGroup._id } });
+        if (req.body != null && req.body.name.trim() != '') {
+            const employer = req.session.profile;
+            req.body.ownerId = employer._id;
+            const newGroup = await GroupModel.create(req.body);
+            req.session.groupNames.push(newGroup);
+            await EmployerModel.findByIdAndUpdate(employer._id, {
+                $push: { groupIDs: newGroup._id }
+            });
+            req.session.save();
+            res.render('profile', {
+                type: req.session.type,
+                groups: req.session.groupNames,
+                numberOfGroups: req.session.groupNames.length,
+                name: req.session.profile.name,
+                loggedIn: req.session.loggedIn
+            });
+        } else {
+            throw new Error('Non null non empty name needed');
+        }
+    } catch (e) {
+        console.log(e);
         res.render('profile', {
             type: req.session.type,
             groups: req.session.groupNames,
             numberOfGroups: req.session.groupNames.length,
             name: req.session.profile.name,
-            loggedIn: req.session.loggedIn
+            loggedIn: req.session.loggedIn,
+            message: 'Please enter a job name!'
         });
-    } catch (e) {
-        console.log(e);
     }
 };
 
 exports.addJob = async (req, res, next) => {
-    const employee = req.session.profile;
-    const joinGroup = await GroupModel.findById(req.body.groupID);
-    await EmployeeModel.findByIdAndUpdate(employee._id, { $push: { groupIDs: joinGroup._id } });
-    await GroupModel.findByIdAndUpdate(joinGroup._id, { $push: { memberIDs: employee._id } });
-    req.session.groupNames.push(joinGroup);
-    res.render('profile', {
-        type: req.session.type,
-        groups: req.session.groupNames,
-        numberOfGroups: req.session.groupNames.length,
-        name: req.session.profile.name,
-        loggedIn: req.session.loggedIn
-    });
+    try {
+        if (req.body != null && req.body.groupID.trim() != '') {
+            const employee = req.session.profile;
+            const joinGroup = await GroupModel.findById(req.body.groupID);
+            await EmployeeModel.findByIdAndUpdate(employee._id, {
+                $push: { groupIDs: joinGroup._id }
+            });
+            await GroupModel.findByIdAndUpdate(joinGroup._id, {
+                $push: { memberIDs: employee._id }
+            });
+            req.session.groupNames.push(joinGroup);
+            req.session.save();
+            res.render('profile', {
+                type: req.session.type,
+                groups: req.session.groupNames,
+                numberOfGroups: req.session.groupNames.length,
+                name: req.session.profile.name,
+                loggedIn: req.session.loggedIn
+            });
+        } else {
+            throw new Error('Null or Empty');
+        }
+    } catch (e) {
+        console.log(e);
+        res.render('profile', {
+            type: req.session.type,
+            groups: req.session.groupNames,
+            numberOfGroups: req.session.groupNames.length,
+            name: req.session.profile.name,
+            loggedIn: req.session.loggedIn,
+            message: 'Please enter a valid group id'
+        });
+    }
 };
 
 exports.employeeJobPage = async (req, res, next) => {
     try {
         if (req.session.profile) {
-            req.session.currentGroup = await GroupModel.findById(req.query.groupID);
+            if (req.query != null) {
+                req.session.currentGroup = await GroupModel.findById(req.query.groupID);
+            }
             req.session.groupSkillNames = await SkillModel.find(
                 { _id: { $in: req.session.currentGroup.skillIDs } },
                 { name: 1 }
@@ -80,7 +122,10 @@ exports.employeeJobPage = async (req, res, next) => {
                 loggedIn: req.session.loggedIn
             });
         } else {
-            res.render('index', { data: 'error, not logged in, please log in', loggedIn: req.session.loggedIn });
+            res.render('index', {
+                data: 'error, not logged in, please log in',
+                loggedIn: req.session.loggedIn
+            });
         }
     } catch (e) {
         console.log(e);
@@ -90,8 +135,9 @@ exports.employeeJobPage = async (req, res, next) => {
 exports.employerJobPage = async (req, res, next) => {
     try {
         if (req.session.profile) {
-            if (req.query.groupID != null)
+            if (req.query.groupID != null) {
                 req.session.currentGroup = await GroupModel.findById(req.query.groupID);
+            }
             req.session.groupSkillNames = await SkillModel.find(
                 { _id: { $in: req.session.currentGroup.skillIDs } },
                 { name: 1 }
@@ -101,6 +147,7 @@ exports.employerJobPage = async (req, res, next) => {
                 { name: 1 }
             );
 
+            req.session.save();
             res.render('groupPage', {
                 jobCode: req.session.currentGroup._id,
                 type: req.session.type,
@@ -110,7 +157,10 @@ exports.employerJobPage = async (req, res, next) => {
                 loggedIn: req.session.loggedIn
             });
         } else {
-            res.render('index', { data: 'error, not logged in, please log in', loggedIn: req.session.loggedIn });
+            res.render('index', {
+                data: 'error, not logged in, please log in',
+                loggedIn: req.session.loggedIn
+            });
         }
     } catch (e) {
         console.log(e);
@@ -119,11 +169,139 @@ exports.employerJobPage = async (req, res, next) => {
 
 exports.createSkill = async (req, res, next) => {
     try {
-        const newSkill = await SkillModel.create(req.body);
-        await GroupModel.findByIdAndUpdate(req.session.currentGroup._id, {
-            $push: { skillIDs: newSkill._id }
+        if (req.body != null && req.body.name.trim() != '') {
+            const newSkill = await SkillModel.create(req.body);
+            await GroupModel.findByIdAndUpdate(req.session.currentGroup._id, {
+                $push: { skillIDs: newSkill._id }
+            });
+            req.session.groupSkillNames.push(newSkill);
+            req.session.save();
+            res.render('groupPage', {
+                jobCode: req.session.currentGroup._id,
+                type: req.session.type,
+                skills: req.session.groupSkillNames,
+                employees: req.session.groupEmployeeNames,
+                name: req.session.currentGroup.name,
+                loggedIn: req.session.loggedIn
+            });
+        } else {
+            throw new Error('Need a non null non empty skill name');
+        }
+    } catch (e) {
+        console.log(e);
+        res.render('groupPage', {
+            jobCode: req.session.currentGroup._id,
+            type: req.session.type,
+            skills: req.session.groupSkillNames,
+            employees: req.session.groupEmployeeNames,
+            name: req.session.currentGroup.name,
+            loggedIn: req.session.loggedIn,
+            message: 'Your skill needs a name'
         });
-        req.session.groupSkillNames.push(newSkill);
+    }
+};
+
+//needs fixing using a patch through employee group page
+exports.employeeUpdateSkill = async (req, res, next) => {
+    try {
+        if (req.query.skillUpdate == 'add') {
+            await EmployeeModel.findByIdAndUpdate(req.session.profile._id, {
+                $push: { skillIDs: req.query.skillID }
+            });
+        } else {
+            await EmployeeModel.findByIdAndUpdate(req.session.profile._id, {
+                $pull: { skillIDs: req.query.skillID }
+            });
+        }
+
+        req.session.profile = await EmployeeModel.findById(req.session.profile._id);
+
+        req.session.save();
+        res.render('groupPage', {
+            jobCode: req.session.currentGroup._id,
+            type: req.session.type,
+            employee: req.session.profile,
+            skills: req.session.groupSkillNames,
+            name: req.session.currentGroup.name,
+            loggedIn: req.session.loggedIn
+        });
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+//need to work on doing calculations client side and sending a patch request
+//also work on making it impossible to enter in a larger number to start then end
+exports.updateAvailability = async (req, res, next) => {
+    try {
+        var monAvail = [],
+            tueAvail = [],
+            wedAvail = [],
+            thAvail = [],
+            friAvail = [],
+            satAvail = [],
+            sunAvail = [];
+
+        function formatTimes(times) {
+            monAvail.push(times[0]);
+            tueAvail.push(times[1]);
+            wedAvail.push(times[2]);
+            thAvail.push(times[3]);
+            friAvail.push(times[4]);
+            satAvail.push(times[5]);
+            sunAvail.push(times[6]);
+        }
+
+        formatTimes(req.body.HoursStart);
+        formatTimes(req.body.MinutesStart);
+        formatTimes(req.body.HalfStart);
+        formatTimes(req.body.HoursEnd);
+        formatTimes(req.body.MinutesEnd);
+        formatTimes(req.body.HalfEnd);
+
+        req.body.monAvail = monAvail;
+        req.body.tueAvail = tueAvail;
+        req.body.wedAvail = wedAvail;
+        req.body.thAvail = thAvail;
+        req.body.friAvail = friAvail;
+        req.body.satAvail = satAvail;
+        req.body.sunAvail = sunAvail;
+
+        await EmployeeModel.findByIdAndUpdate(req.session.profile._id, req.body);
+        req.session.profile = await EmployeeModel.findById(req.session.profile._id);
+        req.session.save();
+        res.render('groupPage', {
+            jobCode: req.session.currentGroup._id,
+            type: req.session.type,
+            employee: req.session.profile,
+            skills: req.session.groupSkillNames,
+            name: req.session.currentGroup.name,
+            loggedIn: req.session.loggedIn
+        });
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+//need to work on getting delete request made
+exports.deleteSkillPage = async (req, res) => {
+    try {
+        await GroupModel.findByIdAndUpdate(req.session.currentGroup._id, {
+            $pull: { skillIDs: req.body.skillID }
+        });
+        await EmployeeModel.updateMany(
+            {
+                $and: [
+                    { _id: { $in: req.session.groupEmployeeNames._id } },
+                    { skillIDs: req.body.skillID }
+                ]
+            },
+            { $pull: { skillIDs: req.body.skillID } }
+        );
+        removeSkill = await SkillModel.findByIdAndDelete(req.body.skillID);
+        req.session.groupSkillNames.splice(req.session.groupSkillNames.indexOf(removeSkill));
+
+        req.session.save();
         res.render('groupPage', {
             jobCode: req.session.currentGroup._id,
             type: req.session.type,
@@ -137,123 +315,34 @@ exports.createSkill = async (req, res, next) => {
     }
 };
 
-exports.employeeUpdateSkill = async (req, res, next) => {
-    if (req.query.skillUpdate == 'add') {
-        await EmployeeModel.findByIdAndUpdate(req.session.profile._id, {
-            $push: { skillIDs: req.query.skillID }
-        });
-    } else {
-        await EmployeeModel.findByIdAndUpdate(req.session.profile._id, {
-            $pull: { skillIDs: req.query.skillID }
-        });
-    }
-
-    req.session.profile = await EmployeeModel.findById(req.session.profile._id);
-
-    res.render('groupPage', {
-        jobCode: req.session.currentGroup._id,
-        type: req.session.type,
-        employee: req.session.profile,
-        skills: req.session.groupSkillNames,
-        name: req.session.currentGroup.name,
-        loggedIn: req.session.loggedIn
-    });
-};
-
-exports.updateAvailability = async (req, res, next) => {
-    var monAvail = [],
-        tueAvail = [],
-        wedAvail = [],
-        thAvail = [],
-        friAvail = [],
-        satAvail = [],
-        sunAvail = [];
-
-    function formatTimes(times) {
-        monAvail.push(times[0]);
-        tueAvail.push(times[1]);
-        wedAvail.push(times[2]);
-        thAvail.push(times[3]);
-        friAvail.push(times[4]);
-        satAvail.push(times[5]);
-        sunAvail.push(times[6]);
-    }
-
-    formatTimes(req.body.HoursStart);
-    formatTimes(req.body.MinutesStart);
-    formatTimes(req.body.HalfStart);
-    formatTimes(req.body.HoursEnd);
-    formatTimes(req.body.MinutesEnd);
-    formatTimes(req.body.HalfEnd);
-
-    req.body.monAvail = monAvail;
-    req.body.tueAvail = tueAvail;
-    req.body.wedAvail = wedAvail;
-    req.body.thAvail = thAvail;
-    req.body.friAvail = friAvail;
-    req.body.satAvail = satAvail;
-    req.body.sunAvail = sunAvail;
-
-    await EmployeeModel.findByIdAndUpdate(req.session.profile._id, req.body);
-    req.session.profile = await EmployeeModel.findById(req.session.profile._id);
-    res.render('groupPage', {
-        jobCode: req.session.currentGroup._id,
-        type: req.session.type,
-        employee: req.session.profile,
-        skills: req.session.groupSkillNames,
-        name: req.session.currentGroup.name,
-        loggedIn: req.session.loggedIn
-    });
-};
-
-exports.deleteSkillPage = async (req, res) => {
-    await GroupModel.findByIdAndUpdate(req.session.currentGroup._id, {
-        $pull: { skillIDs: req.body.skillID }
-    });
-    await EmployeeModel.updateMany(
-        {
-            $and: [
-                { _id: { $in: req.session.groupEmployeeNames._id } },
-                { skillIDs: req.body.skillID }
-            ]
-        },
-        { $pull: { skillIDs: req.body.skillID } }
-    );
-    removeSkill = await SkillModel.findByIdAndDelete(req.body.skillID);
-    req.session.groupSkillNames.splice(req.session.groupSkillNames.indexOf(removeSkill));
-
-    res.render('groupPage', {
-        jobCode: req.session.currentGroup._id,
-        type: req.session.type,
-        skills: req.session.groupSkillNames,
-        employees: req.session.groupEmployeeNames,
-        name: req.session.currentGroup.name,
-        loggedIn: req.session.loggedIn
-    });
-};
-
 exports.getUpdateSkillPage = async (req, res) => {
-    req.session.currentSkill = await SkillModel.findById(req.query.skillID);
-
-    console.log(req.session.currentGroup);
-    res.render('skillPage', {
-        loggedIn: req.session.loggedIn,
-        jobCode: req.session.currentGroup._id,
-        skill: req.session.currentSkill
-    });
+    try {
+        req.session.currentSkill = await SkillModel.findById(req.query.skillID);
+        if (req.session.currentSkill == null) {
+            res.render('groupPage', {
+                jobCode: req.session.currentGroup._id,
+                type: req.session.type,
+                skills: req.session.groupSkillNames,
+                employees: req.session.groupEmployeeNames,
+                name: req.session.currentGroup.name,
+                loggedIn: req.session.loggedIn,
+                message: 'cannot find this skill'
+            });
+        }
+        req.session.save();
+        res.render('skillPage', {
+            loggedIn: req.session.loggedIn,
+            jobCode: req.session.currentGroup._id,
+            skill: req.session.currentSkill
+        });
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 exports.updateSkillPage = async (req, res) => {
     try {
         const test = await SkillModel.findByIdAndUpdate(req.session.currentSkill._id, req.body);
-
-        /*res.render('groupPage', {*/
-        //jobCode: req.session.currentGroup._id,
-        //type: req.session.type,
-        //skills: req.session.groupSkillNames,
-        //employees: req.session.groupEmployeeNames,
-        //name: req.session.currentGroup.name
-        /*});*/
         res.status(200).json({
             status: 'success',
             data: {
