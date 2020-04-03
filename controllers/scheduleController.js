a3eae3d99c63cc921c7837a73915c4e3b55e0928
Fred York <fredwashere53@gmail.com>
2020-04-03 17:56:26 -0400

    created scheduleController.js
    this is where the bulk of our scheduling algorithm will be located once we start building it

---
+++ controllers/scheduleController.js
@@ -0,0 +1,13 @@
+const EmployeeModel = require('./../models/employee');
+const EmployerModel = require('./../models/employer');
+const GroupModel = require('./../models/group');
+const SkillModel = require('./../models/skill');
+
+exports.generateSchedule = async (req, res, next) => {
+    //start building the algorithm here
+    try {
+
+    } catch (e) {
+        console.log(e);
+    }
+}
