b604424e15f53dacd702054e619403f6bd525f74
muma10837 <jmuma@uncc.edu>
2020-04-06 09:47:27 -0400

    should have a list of all employees who are +- 2 hours from shift start/end

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -3,38 +3,69 @@ const EmployerModel = require('./../models/employer');
 const GroupModel = require('./../models/group');
 const SkillModel = require('./../models/skill');
 
+/*var x; //members*/
+//var y; //skills
+//var z; // the end product that will be returned
+//for (x in currentMembers) {
+//for (y in currentSkills) {
+////compare the time specified by the employer to the
+////availability of the employee
+////do this for all days and check whether the day is
+////enabled or not
+//}
+/*}*/
 exports.generateSchedule = async (req, res, next) => {
     //start building the algorithm here
     try {
+        const days = ['mon', 'tues', 'wed', 'th', 'fri', 'satur', 'sun'];
         var currentSkills = req.session.currentGroup.skillIDs;
         var currentMembers = req.session.currentGroup.memberIDs;
-        var x;//members
-        var y;//skills
-        var z;// the end product that will be returned
-        for(x in currentMembers){
-            for(y in currentSkills){
-                //compare the time specified by the employer to the
-                //availability of the employee
-                //do this for all days and check whether the day is
-                //enabled or not
 
+        async function daySchedule(day) {
+            var counter = 0;
+            var employeeDay = [];
+            const dayShift = day + 'Shift';
+            const dayAvail = day + 'Avail';
+            for (const skill in currentSkills) {
+                console.log(skill[dayShift[0]]);
+                employeeDay.push(
+                    await EmployeeModel.find({
+                        $and: [
+                            { _id: { $in: skill.userIDs } },
+                            { [dayAvail[0]]: { $gte: skill[dayShift[0]] - 2 } },
+                            { [dayAvail[0]]: { $lte: skill[dayShift[0]] + 2 } },
+                            { [dayAvail[2]]: { $eq: skill[dayShift[2]] } },
+                            { [dayAvail[3]]: { $gte: skill[dayShift[3]] - 2 } },
+                            { [dayAvail[3]]: { $lte: skill[dayShift[3]] + 2 } },
+                            { [dayAvail[5]]: { $eq: skill[dayShift[5]] } }
+                        ]
+                    })
+                );
+                counter++;
             }
         }
+
+        const monSchedule = daySchedule(days[0]);
+        const tueSchedule = daySchedule(days[1]);
+        const wedSchedule = daySchedule(days[2]);
+        const thSchedule = daySchedule(days[3]);
+        const friSchedule = daySchedule(days[4]);
+        const satSchedule = daySchedule(days[5]);
+        const sunSchedule = daySchedule(days[6]);
     } catch (e) {
         console.log(e);
     }
-}
-
+};
 
 //might not need this section, will figure it out later, for now going to
 //work on the actual algorithm
 exports.showScheduleMaker = async (req, res, next) => {
     try {
-        if(req.session.profile) {
+        if (req.session.profile) {
             //don't even know if we need this page, might be better to just run the
             //algorithm then find out whether or not the schedule can be made
-            if(req.query.groupId != null){
-                console.log("group ID: " + req.query.groupId);
+            if (req.query.groupId != null) {
+                console.log('group ID: ' + req.query.groupId);
                 req.session.currentGroup = await GroupModel.findById(req.query.groupID);
             }
             req.session.groupSkillNames = await SkillModel.find(
@@ -47,4 +78,5 @@ exports.showScheduleMaker = async (req, res, next) => {
     } catch (e) {
         console.log(e);
     }
-}
+};
+
